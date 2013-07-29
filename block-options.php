df3de21c2b8afdc8447d5364c741ea86ccdd8c39
ch0ck0b3ar <ch0ck0b3ar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-29 14:56:27 +0000

    Uploading working files.
    git-svn-id: https://plugins.svn.wordpress.org/headway-image-block/tags/1.1@747850 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ block-options.php
@@ -0,0 +1,45 @@
+<?php
+class HeadwayImageBlockOptions extends HeadwayBlockOptionsAPI {	
+	public $tabs = array(
+			'settings' => 'Settings'
+	);
+	public $inputs = array(
+			'settings' => array(
+			'image-block-image' => array(
+				'type' => 'image',
+				'name' => 'image-block-image',
+				'label' => 'Image',
+				'default' => '',
+				'tooltip' => 'Upload your image.'
+			),
+			'image-block-alt' => array(
+				'type' => 'text',
+				'name' => 'image-block-alt',
+				'label' => 'Image Alt',
+				'default' => '',
+				'tooltip' => 'Add alternate text.'
+			),
+			'image-block-resize' => array(
+				'type' => 'checkbox',
+				'name' => 'image-block-resize',
+				'label' => 'Resize Image?',
+				'default' => false,
+				'tooltip' => 'Resize image to fit block dimensions'
+			),
+			'image-block-link' => array(
+				'type' => 'text',
+				'name' => 'image-block-link',
+				'label' => 'Image Link',
+				'default' => '',
+				'tooltip' => 'URL to link the image to.'
+			),
+			'image-block-target' => array(
+				'type' => 'checkbox',
+				'name' => 'image-block-target',
+				'label' => 'Open Link in New Window',
+				'default' => true,
+				'tooltip' => 'Open link in new window?'
+			)
+		)
+	);
+}
